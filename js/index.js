386655c41f032a875dd811ad5a12795a6d224ac6
huai-sian <bpud50122@gmail.com>
2021-10-24 17:27:26 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,509 @@
+const myModal = new bootstrap.Modal(document.getElementById('trial-modal'), {
+  keyboard: false
+});
+myModal.show();
+
+const data = {
+  "Top Hits":[
+    {
+      "album": "No song without you",
+      "singer": "Honne",
+      "type": "Top Hits",
+      "img": "./no_song_without_you.jpeg",
+      "backgroundImg": "images/10428166_468795979923968_4635514652121751513_o.jpeg",
+      "followers": "1,914,830",
+      "isFollow": false,
+      "copyright": "© 2019 MNH ENTERTAINMENT",
+      "songs": [
+        {
+          "song": "No song without you",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "02:59",
+          "ytId": "WXOlNBDVt0o",
+          "liked": false
+        },
+        {
+          "song": "Free love",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "03:31",
+          "ytId": "w-EoAXhKOLk",
+          "liked": false
+        },
+        {
+          "song": "la la la that's how it goes",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "03:40",
+          "ytId": "43FgE6uCyLw",
+          "liked": false
+        },
+        {
+          "song": "gone gone gone",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "03:33",
+          "ytId": "zL5tqEO2zwg",
+          "liked": false
+        }
+      ]
+    },
+    {
+      "album": "Love me / Love me not",
+      "singer": "Honne",
+      "type": "Top Hits",
+      "img": "./love_me_or_not.jpeg",
+      "backgroundImg": "images/10428166_468795979923968_4635514652121751513_o.jpeg",
+      "followers": "1,914,830",
+      "isFollow": false,
+      "copyright": "© 2019 MNH ENTERTAINMENT",
+      "songs": [
+        {
+          "song": "Me and You",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "04:03",
+          "ytId": "SB4Jra4zR80",
+          "liked": false
+        },
+        {
+          "song": "Location Unknown",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "05:25",
+          "ytId": "btIQvYcLNoI",
+          "liked": false
+        },
+        {
+          "song": "Crying Over you",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "03:59",
+          "ytId": "GdJP11JwLos",
+          "liked": false
+        },
+        {
+          "song": "Day 1",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "03:53",
+          "ytId": "hWOB5QYcmh0",
+          "liked": false
+        }
+      ]
+    }
+  ],
+  "Taiwan Hits": [
+    {
+      "album": "11月的蕭邦",
+      "singer": "Jay Chou",
+      "type": "Taiwan Hits",
+      "img": "./R-3227320-1609179445-6309.jpeg.jpg",
+      "backgroundImg": "images/kkMaIs4wzIE357wu2AYhxcVWp8SWbV1oeTOGf3kzhn8.jpeg",
+      "followers": "1,914,830",
+      "isFollow": false,
+      "copyright": "© 2019 MNH ENTERTAINMENT",
+      "songs": [
+        {
+          "song": "珊瑚海",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "04:24",
+          "ytId": "kYhh1PpsOg4",
+          "liked": false
+        },
+        {
+          "song": "一路向北",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "05:02",
+          "ytId": "L229QDxDakU",
+          "liked": false
+        },
+        {
+          "song": "黑色毛衣",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "04:13",
+          "ytId": "bX33UI9ZPLk",
+          "liked": false
+        },
+        {
+          "song": "髮如雪",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "05:01",
+          "ytId": "aaM7qG2ycjk",
+          "liked": false
+        }
+      ]
+    },
+    {
+      "album": "七里香",
+      "singer": "Jay Chou",
+      "type": "Taiwan Hits",
+      "img": "./jaychou.jpeg",
+      "backgroundImg": "images//kkMaIs4wzIE357wu2AYhxcVWp8SWbV1oeTOGf3kzhn8.jpeg",
+      "followers": "1,914,830",
+      "isFollow": false,
+      "copyright": "© 2019 MNH ENTERTAINMENT",
+      "songs": [
+        {
+          "song": "七里香",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "05:03",
+          "ytId": "Bbp9ZaJD_eA",
+          "liked": false
+        },
+        {
+          "song": "擱淺",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "04:28",
+          "ytId": "YJfHuATJYsQ",
+          "liked": false
+        },
+        {
+          "song": "藉口",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "04:29",
+          "ytId": "KcK8WurGpEQ",
+          "liked": false
+        },
+        {
+          "song": "外婆",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "04:09",
+          "ytId": "Ur-x4pZT1Rk",
+          "liked": false
+        }
+      ]
+    }
+  ],
+  "Spain Hits": [
+    {
+      "album": "Eterno Agosto",
+      "singer": "Alvaro Soler",
+      "type": "Spanish Hits",
+      "img": "./Eterno-Agosto-by-Alvaro-Soler.jpeg",
+      "backgroundImg": "images//titel-soler-b.jpeg",
+      "followers": "1,914,830",
+      "isFollow": false,
+      "copyright": "© 2019 MNH ENTERTAINMENT",
+      "songs": [
+        {
+          "song": "Sofia",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "03:33",
+          "ytId": "qaZ0oAh4evU",
+          "liked": false
+        },
+        {
+          "song": "Libre",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "03:56",
+          "ytId": "OMBVoxgZooE",
+          "liked": false
+        },
+        {
+          "song": "Animal",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "03:57",
+          "ytId": "YfDhHUuGMuo",
+          "liked": false
+        },
+        {
+          "song": "El mismo sol",
+          "videoId": "https://yildirimzlm.s3.us-east-2.amazonaws.com/Post+Malone+-+rockstar+ft.+21+Savage+(Official+Audio).mp3",
+          "duration": "03:20",
+          "ytId": "aNHwNreDp3A",
+          "liked": false
+        }
+      ]
+    }
+  ]
+};
+
+const nav_home = document.querySelector('.sidebar__link--home');
+const nav_list = document.querySelector('.sidebar__link--playlist');
+const sec_home = document.querySelector('.sec-home');
+const sec_list = document.querySelector('.sec-playlist');
+const play = document.querySelector('.icon__play');
+const prev = document.querySelector('.fa-backward');
+const next = document.querySelector('.fa-forward');
+const footer_img = document.querySelector('.footer__thumbnail');
+const footer_singer = document.querySelector('.footer__txtbox p');
+const footer_song = document.querySelector('.footer__txtbox h3');
+const audio = document.querySelector('.footer__txtbox audio');
+const progressBar = document.querySelector('.footer__music-progress-bar');
+const progressZone = document.querySelector('.footer__music-progress');
+const musicCurrentTime = document.querySelector('.music-current-time');
+const musicDurationTime = document.querySelector('.music-duration-time');
+const repeat = document.querySelector('.icon__repeat');
+const continuous = document.querySelector('.icon__shuffle');
+const play_btn = document.querySelector('.playlist__top__play');
+const follow = document.querySelector('.playlist__top__follow');
+const volume = document.querySelector('.footer__volumn');
+const mute = document.querySelector('.footer__mute');
+
+let album = data['Top Hits'][0];
+let musicIdx = 0;
+let isPlay = false;
+let repeatFlag = false;
+let continuousFlag = false;
+let defaultVolume = .7;
+let isMute = false;
+
+(function(){
+  volume.value = defaultVolume * 100;
+  audio.volume = defaultVolume;
+}());
+
+mute.addEventListener('click', function() {
+  isMute = !isMute;
+  console.log('mute test');
+  if(isMute == true) {
+    mute.classList.replace('fa-volume-down', 'fa-volume-off');
+    volume.value = 0;
+    audio.volume = 0;
+  } else {
+    mute.classList.replace('fa-volume-off', 'fa-volume-down');
+    volume.value = defaultVolume * 100;
+    audio.volume = defaultVolume;
+  }
+})
+
+volume.addEventListener('change', function() {
+  let nowVolume = (volume.value) / 100;
+  audio.volume = nowVolume;
+})
+
+nav_home.addEventListener('click', function() {
+  sec_home.classList.remove('d-none');
+  sec_list.classList.add('d-none');
+  sec_list.classList.remove('active');
+  sec_home.classList.add('active');
+});
+
+nav_list.addEventListener('click', function() {
+  sec_home.classList.add('d-none');
+  sec_list.classList.remove('d-none');
+  sec_list.classList.add('active');
+  sec_home.classList.remove('active');
+});
+
+follow.addEventListener('click', function() {
+  console.log(this.textContent);
+  let tempFollowers = parseInt(album.followers.replace(/,/g, ''), 10);
+  //this.textContent = '';
+  if(this.textContent === 'FOLLOW'){
+    tempFollowers += 1;
+    $('.playlist__top__followers').text(`${tempFollowers.toLocaleString('en-us')} FOLLOWERS`);
+    this.textContent = 'UNFOLLOW';
+  } else {
+    tempFollowers -= 0;
+    $('.playlist__top__followers').text(`${tempFollowers.toLocaleString('en-us')} FOLLOWERS`);
+    this.textContent = 'FOLLOW';
+  }
+});
+
+renderType();
+renderAlbum();
+loadMusic(album, musicIdx);
+loadPlaylist(album);
+
+play.addEventListener('click', () => {
+  isPlay ? pauseMusic() : playMusic();
+});
+
+play_btn.addEventListener('click', () => {
+  isPlay ? pauseMusic() : playMusic();
+})
+
+function renderType() {
+  const typeList = Object.keys(data);
+  console.log(typeList);
+  let str = '';
+  for(let i = 0; i < typeList.length; i++) {
+    str += `<h2 class="albumList__h2 mb-3">${typeList[i]}</h2>
+              <div class="row mb-4 albumList__row albumList__row--${i}">
+              </div>`;
+  }
+  $('.albumList').html(str);
+}
+
+function renderAlbum() {
+  const typeList = Object.keys(data);
+  for(let i = 0; i < typeList.length; i++) {
+    let albumStr = '';
+    for(let j = 0; j < data[typeList[i]].length; j++) {
+      albumStr +=  `<div class="col-12 col-md-4 mb-3">
+                      <div class="albumList__img mx-auto mb-2"
+                        style="background-image: url(images/${data[typeList[i]][j].img})" data-idx="${j}" data-type="${typeList[i]}"></div>
+                      <p class="text-white albumList__name">${data[typeList[i]][j].album}</p>
+                    </div>`;
+    }
+    console.log($(`.albumList__row--${i}`));
+  $(`.albumList__row--${i}`).html(albumStr);
+  }
+  const albumList__img = document.querySelectorAll('.albumList__img');
+  console.log(albumList__img.length);
+  albumList__img.forEach(item => {
+    item.addEventListener('click', directToSingle);
+  })
+}
+
+function loadMusic(album, idx) {
+  footer_img.style.backgroundImage = `url(images/${album.img})`;
+  footer_singer.textContent = album.singer;
+  footer_song.textContent = album.songs[idx].song;
+  audio.src = album.songs[idx].videoId;
+}
+
+function playMusic() {
+  audio.play();
+  isPlay = true;
+  play.classList.replace('fa-play', 'fa-pause');
+  play_btn.textContent = 'PAUSE';
+}
+
+function pauseMusic() {
+  audio.pause();
+  isPlay = false;
+  play.classList.replace('fa-pause', 'fa-play');
+  play_btn.textContent = 'PLAY';
+}
+
+function updateProgress(e) {
+  const {
+    duration,
+    currentTime,
+  } = e.srcElement;
+  //console.log(currentTime);
+  const progressPercent = (currentTime / duration) * 100;
+  progressBar.style.width = `${progressPercent}%`;
+  if(progressPercent == 100 && repeatFlag) {
+    loadMusic(album, musicIdx);
+    audio.duration = 0;
+    isPlay = true;
+    audio.play();
+  } else if (progressPercent == 100 && continuousFlag) {
+    nextMusic();
+  }
+}
+
+function setTime(e) {
+  const {
+    duration,
+    currentTime,
+  } = e.srcElement;
+  calcTime(duration, musicDurationTime);
+  calcTime(currentTime, musicCurrentTime);
+}
+
+function calcTime(time, el) {
+  time = Number(time);
+  const m = Math.floor(time % 3600 / 60);
+  const s = Math.floor(time % 3600 % 60);
+  return el.textContent = `${m < 10 ? '0' : ''}${m}:${s < 10 ? '0' : ''}${s}`
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const setPoint = e.offsetX;
+  const duration = audio.duration;
+  audio.currentTime = (setPoint / width) * duration;
+}
+
+function prevMusic() {
+  musicIdx = (musicIdx - 1 + album.songs.length) % album.songs.length;
+  loadMusic(album, musicIdx);
+  audio.duration = 0;
+  if (isPlay) {
+    audio.play();
+  }
+}
+
+function nextMusic() {
+  musicIdx = (musicIdx + 1 + album.songs.length) % album.songs.length;
+  loadMusic(album, musicIdx);
+  audio.duration = 0;
+  if (isPlay) {
+    audio.play();
+  }
+}
+
+function repeatMusic() {
+  repeatFlag = !repeatFlag;
+  if(repeatFlag){
+    repeat.style.color = '#26de85';
+  } else {
+    repeat.style.color = 'white';
+  }
+}
+
+function continuousMusic() {
+  continuousFlag = !continuousFlag;
+}
+
+function directToSingle() {
+  let tempidx = this.dataset.idx;
+  let tempType = this.dataset.type;
+  album = data[tempType][tempidx];
+  musicIdx = 0;
+  sec_home.classList.add('d-none');
+  sec_list.classList.remove('d-none');
+  loadMusic(album, musicIdx);
+  audio.duration = 0;
+  playMusic();
+  console.log(album);
+  loadPlaylist(album);
+}
+
+function loadPlaylist(album) {
+  $('.playlist__top__followers').text(`${album.followers} FOLLOWERS`);
+  $('.playlist__top__singer').text(album.singer);
+  $('.playlist__top').css('background-image', `url(${album.backgroundImg})`)
+  let listStr = '';
+  for(let i = 0; i < album.songs.length; i++) {
+    listStr += `<li class="d-flex justify-content-between align-items-center mb-3 playlist__li" data-song="${album.songs[i]}" data-index="${i}">
+                  <div class="d-flex align-items-center">
+                    <small class="playlist__li__num me-2">${i + 1}</small>
+                    <span class="playlist__li__name">${album.songs[i].song}</span>
+                  </div>
+                  <div class="d-flex align-items-center justify-content-around">
+                    <small class="playlist__li__duration">${album.songs[i].duration}</small>
+                    <div class="mx-2">
+                      ${album.songs[i].liked ? `<i class="material-icons text-success icon-like">favoriter</i>`: `<i class="material-icons icon-like">favorite_border</i>`}
+                    </div>
+                    <div class="position-relative">
+                      <i class="material-icons more__youtube" onclick="directTo('${album.songs[i].ytId}')">more_horiz</i>
+                      <a class="playlist__li__youtube d-none">Youtube</a>
+                    </div>
+                  </div>
+                </li>`;
+  }
+  $('.playlist__ul').html(listStr);
+  $('.more__youtube').click(function() {
+    $(this).next().toggleClass('d-none');
+  });
+  $('.icon-like').click(toggleLike);
+  $('.playlist__li').click(changeSong);
+}
+
+function toggleLike() {
+  console.log($(this));
+  if($(this).text() === 'favoriter') {
+    $(this).text('favorite_border');
+  } else {
+    $(this).text('favoriter');
+  }
+}
+
+function changeSong() {
+  console.log(parseInt($(this).data('index')));
+  musicIdx = parseInt($(this).data('index'));
+  loadMusic(album, musicIdx);
+  audio.duration = 0;
+  playMusic();
+}
+
+function directTo(id) {
+  window.location.href = `https://www.youtube.com/watch?v=${id}`;
+}
+
+audio.addEventListener('timeupdate', updateProgress);
+audio.addEventListener('timeupdate', setTime);
+progressZone.addEventListener('click', setProgress);
+prev.addEventListener('click', prevMusic);
+next.addEventListener('click', nextMusic);
+repeat.addEventListener('click', repeatMusic);
+continuous.addEventListener('click', continuousMusic);
